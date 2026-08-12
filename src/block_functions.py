5c27b82679096444737c9ade0f16a3400924b697
Leonid Karavaev <lekara@DESKTOP-154TFEU.localdomain>
2026-08-12 21:53:16 +0300

    ch4 l1 fixed

--- src/block_functions.py
+++ src/block_functions.py
@@ -3,6 +3,8 @@
 def markdown_to_blocks(markdown: str) -> list[str]:
     new_blocks = []
     for block in markdown.split("\n\n"):
+        if block == "":
+            continue
         new_blocks.append(block.strip())
     return new_blocks
     
